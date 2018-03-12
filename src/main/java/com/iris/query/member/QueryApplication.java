a01d0edfac8d219669f349a638d9a5612c4297c6
dazd <dazd@192.168.3.106>
2018-03-12 21:55:24 +0800

    Initial commit

---
+++ src/main/java/com/iris/query/member/QueryApplication.java
@@ -0,0 +1,12 @@
+package com.iris.query.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryApplication.class, args);
+	}
+}
